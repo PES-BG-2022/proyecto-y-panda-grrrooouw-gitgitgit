3c1190d70a2237a63fbe4c76de882b28bad575ff
martinc97 <macordon@ufm.edu>
2022-01-20 11:38:13 -0600

    Cambiesichz

--- proyect.py
+++ proyect.py
@@ -39,3 +39,56 @@ except:
 #time.sleep(5)
 #driver.quit()
 
+#*************************************************************************************************************
+
+# DATE
+
+def date(y, m, d):
+
+    period = int(time.mktime(datetime.datetime(y, m, d, 23, 59).timetuple()))
+    return period
+
+date1 = date(2020, 1, 1)
+date2 = date(2021, 12, 31)
+
+# QUERY
+
+ticker = 'AAPL'
+interval = '1mo' # 1d, 1wk
+
+def fin_data(ticker: str, date1: int, date2: int, interval: str):
+
+    qstr = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={date1}&period2={date2}&interval={interval}&events=history&includeAdjustedClose=true'
+
+    return pd.read_csv(qstr)
+
+# CANDLESTICK
+
+def candle(df):
+
+    fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                    open=df['Open'],
+                    high=df['High'],
+                    low=df['Low'],
+                    close=df['Close'])])
+
+    fig_w = go.FigureWidget(fig)
+
+    return fig_w
+
+# SAVE
+
+def save(df):
+
+    df.to_excel(f'historical_prices_{ticker}.xlsx', sheet_name=ticker, index=False)
+
+def save_mult():
+
+    writer = pd.ExcelWriter('historical_prices.xlsx')
+
+    for t in tickers:
+
+        df = fin_data(t, date1, date2, interval)
+        df.to_excel(writer, sheet_name=t, index=False)
+
+    writer.save()
