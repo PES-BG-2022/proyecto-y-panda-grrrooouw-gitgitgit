05b33f65428b319a7479aee34362fcf93b131e5f
Erick Antonio Ruíz Galeano <erickruiz@ufm.edu>
2022-01-20 19:18:52 -0600

    Finalización del programa principal y se adjunta el cuaderno de pruebas de trabajo

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 ############# APLICACIÓN DE PRONÓSTICOS ############
 
 import time
+import datetime as dt
 import datetime
 from openpyxl import Workbook
-
 import streamlit as st
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
 
+
 from operator import index
 from io import BytesIO
-# from fbprophet import Prophet
-# from fbprophet.plot import plot_plotly
+from prophet import Prophet
+from prophet.plot import plot_plotly
 
 ### Preliminares
 
@@ -21,14 +22,17 @@ def date(y, m, d):
     period = int(time.mktime(datetime.datetime(y, m, d, 23, 59).timetuple()))
     return period
 
-inicio = date(2000, 1, 1)
+#inicio = date(2000, 1, 1)
 hoy = int(time.mktime(datetime.datetime.now().timetuple()))
 
 # Dashboard
 
-st.title('App de pronósticos')
+st.title('App de pronósticos: Martín Cordón y Erick Ruíz -grrrrrraaaaww GITGITGIT')
 
-ticker = st.text_input('Introduzca el "ticker" del activo que desea analizar')
+inicio = st.date_input('Introduzca una fecha', datetime.datetime(2000, 1, 1, 23, 59))
+inicio2 = int(time.mktime(inicio.timetuple()))
+
+ticker = st.text_input('Introduzca el "ticker" del activo que desea analizar', "PZZA")
 
 interval = st.selectbox('Escoja un intervalo de tiempo', ['1d', '1wk', '1mo'])
 
@@ -50,8 +54,8 @@ else:
 
 @st.cache
 def fin_data(ticker: str):
-
-    qstr = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={inicio}&period2={hoy}&interval={interval}&events=history&includeAdjustedClose=true'
+    
+    qstr = f'https://query1.finance.yahoo.com/v7/finance/download/{ticker}?period1={inicio2}&period2={hoy}&interval={interval}&events=history&includeAdjustedClose=true'
 
     datos = pd.read_csv(qstr)
 
@@ -102,21 +106,21 @@ candle()
 
 # Prophet
 
-# df_train = data[['Date','Close']]
-# df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-# m = Prophet()
-# m.fit(df_train)
-# future = m.make_future_dataframe(periods=periodo)
-# forecast = m.predict(future)
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=periodo)
+forecast = m.predict(future)
 
-# st.subheader('Datos pronosticados')
-# st.write(forecast.tail())
+st.subheader('Datos pronosticados')
+st.write(forecast.tail())
 
-# st.write(f'Proóstico para {n_years} años')
-# fig1 = plot_plotly(m, forecast)
-# st.plotly_chart(fig1)
+st.write(f'Pronóstico para {n_years} años')
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
 
-# st.write("Componentes del pronóstico")
-# fig2 = m.plot_components(forecast)
-# st.write(fig2)
+st.write("Componentes del pronóstico")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
